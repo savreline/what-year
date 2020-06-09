33182e58e8a3c9ae8fdd5f10610e1b9fb97b7d8c
Sasha Avreline <savreline@gmail.com>
2020-06-08 18:23:17 -0700

    Dependencies and Basic Config

---
+++ .eslintrc.js
@@ -0,0 +1,25 @@
+module.exports = {
+  parser: 'babel-eslint',
+  env: {
+    browser: true,
+    commonjs: true,
+    es6: true,
+    node: true,
+    jest: true
+  },
+  extends: ['eslint:recommended', 'plugin:react/recommended'],
+  parserOptions: {
+    ecmaFeatures: {
+      jsx: true,
+    },
+    sourceType: 'module',
+  },
+  plugins: ['react'],
+  rules: {
+    indent: ['error', 2, {SwitchCase: 1}],
+    'linebreak-style': ['error', 'windows'],
+    quotes: ['error', 'single'],
+    semi: ['error', 'always'],
+    'no-console': ['warn', {allow: ['clear', 'info', 'error', 'dir', 'trace']}],
+  }
+};
