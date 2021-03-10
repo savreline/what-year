6feaca4a6382e0e831240bd890e27a31b8b55f7f
Sasha Avreline <savreline@gmail.com>
2021-03-10 13:14:29 -0800

    Added author's note

--- src/components/Start.js
+++ src/components/Start.js
@@ -13,8 +13,9 @@ class Start extends Component {
 
   render() {
     return (
-      <div className="border shadow my-4 p-3 text-justify">
-        <p>
+      <div>
+        <div className="border shadow my-4 p-3 text-justify">
+          <p>
             Welcome to <em>what year was that</em> game. Enter your a name below, 
             pick a category and start playing! For each question, type in your best guess 
             as to in which year the given event took place. You get three tries! After each
@@ -23,21 +24,23 @@ class Start extends Component {
             1000 points for each question, reduced by the amount of tries you used and by how
             far away your best attempt is from the correct answer. Each game session consists
             of 10 questions. Have fun!
-        </p>
-        <p><strong>N.B. </strong>
+          </p>
+          <p><strong>N.B. </strong>
             At this time, if you enter the same name you used before, the points you earned 
             during the current game session will be added to your previous score. You also
             wouldn&apos;t be promted with the questions you already answered. In the future, 
             a username login system would be added to address this issue.
-        </p>
-        <form className="text-center">
-          <input type="text"
-            placeholder="Enter a name..."
-            className="form-control form-control-lg my-3"
-            ref={(c) => { this.nameEntered = c; }} />
-          <Categories 
-            handleSubmit={this.handleSubmit} />        
-        </form>  
+          </p>
+          <form className="text-center">
+            <input type="text"
+              placeholder="Enter a name..."
+              className="form-control form-control-lg my-3"
+              ref={(c) => { this.nameEntered = c; }} />
+            <Categories 
+              handleSubmit={this.handleSubmit} />        
+          </form>  
+        </div>
+      Created by <a href="https://savreline.com">Sasha Avreline</a>
       </div>
     );
   }
